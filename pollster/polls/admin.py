343f8b4da827d07d5018023b53a7c03dd327c76d
Ayeah Godlove <ayeahgodlove5@gmail.com>
2022-07-11 16:09:14 +0100

    complete pollster applicaton with django and python

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the poster admin area"
 # tabular inline
 class ChoiceInline(admin.TabularInline):
     model = Choice
